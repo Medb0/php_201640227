4a763f0dd709006888df53382ddd289517f7508b
Medb0 <bxcv1230@gmail.com>
2019-09-25 16:38:36 +0900

    5주차 수업

---
+++ php/function_10.php
@@ -0,0 +1,7 @@
+<?php
+
+include "function_11.php";
+include "function_11.php";
+
+list($name, $num) = daelim("대림이",123456);
+echo $name . " = " . $name;
---
+++ php/hello_11.php
@@ -0,0 +1,7 @@
+<?php
+  include "hello_10.php";
+  echo "안녕하세요.<br>";
+  include "./hello/hello_12.php";
+  require "./hello/hello_12.php";
+  require "hello_10.php";
+?>
