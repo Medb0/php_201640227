0e05c0ca492c02c95846f559f0589d80cbb52605
Medb0 <bxcv1230@gmail.com>
2019-10-30 18:24:53 +0900

    fix

--- php/db/index.php
+++ php/db/index.php
@@ -1,35 +1,16 @@
 <?php
 
-include 'dbconf.php';
+$config = include 'dbconf.php';
 require 'database.php';
 require 'table.php';
 
-// 순서가 틀릴경우를 대비한 연상배열
-$config = [
-  "host"=>$host,
-  "user"=>$user,
-  "password"=>$password,
-  "database"=>$database];
-$db= new Database($config);
+$db = new Database($config);
+echo "<br>";
+$query = "SHOW TABLES";
+$result = $db->queryExecute($query);
 
-/*
-// 테이블 객체를 생성해서 연결
-$db->setTable(new Table);
-// 연결된 객체를 읽어옴.
-$db->getTable()->createTable();
-*/
-// 메소드체인 기법을 활용한 방법
-//$db->setTable(new Table($db))->getTable()->createTable();
-$db->getTable()->createTable("members3",
-  [
-    'Last'=>"varchar(50)",
-    'First'=>"varchar(50)",
-    'Age'=>"varchar(50)"
-  ]
-);
-
-if ($db->isTable("members")) {
-  echo "테이블이 존재합니다.<br>";
-}else {
-  echo "테이블이 존재하지 않습니다.<br>";
+$count = mysqli_num_rows($result);
+for ($i=0; $i < $count ; $i++) {
+  $row = mysqli_fetch_object($result);
+  echo $row->Tables_in_php."<br>";
 }
